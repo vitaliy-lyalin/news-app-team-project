bb81b88024b113aabd0eaff97aacd517db98ea59
Kate Kolosovska-Shchudlo <kate.shchudlo@gmail.com>
2023-03-03 00:07:31 +0200

    add header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import createRenderCategoriesMarkup from './js/createRenderCategories';
 import showHideOthersCategories from './js/showHideOthersCategories';
 import getCategoriesValue from './js/getCategoriesValue';
+import { onBurgerBtnClick, refs } from './js/mobileBurger';
 
 const categoriesEl = document.querySelector('.filter-wrapper');
 
@@ -13,3 +14,5 @@ categoriesEl.addEventListener('click', event => {
   const categorySelected = getCategoriesValue(event);
   console.log(categorySelected);
 });
+// -> open burger menu
+refs.headerBurger.addEventListener('click', onBurgerBtnClick);
