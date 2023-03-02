8d906c662274cfc0af290f69e303bbfc14dc98ce
Dimon <bolilui.dmytro@gmail.com>
2023-03-02 18:50:20 +0200

    Add functions for: 1) fetch categories 2) do markup and render it 3) return category`s value after click event

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import createRenderCategoriesMarkup from './js/createRenderCategories';
+import showHideOthersCategories from './js/showHideOthersCategories';
+import getCategoriesValue from './js/getCategoriesValue';
+
+const categoriesEl = document.querySelector('.filter-wrapper');
+
+createRenderCategoriesMarkup();
+
+// -> Show - Hide others categories on 'Others' button click
+categoriesEl.addEventListener('click', showHideOthersCategories);
+// -> Get category value after click
+categoriesEl.addEventListener('click', event => {
+  const categorySelected = getCategoriesValue(event);
+  console.log(categorySelected);
+});
