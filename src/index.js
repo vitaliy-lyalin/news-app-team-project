635d722c1dce0339286a0f5fd354fdccf67ce182
Kate Kolosovska-Shchudlo <kate.shchudlo@gmail.com>
2023-03-02 16:23:26 +0200

    add vars to scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const headerBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
-headerBurger.addEventListener('click', function () {
+const logo = document.querySelector('.header__logo');
+const body = document.querySelector('body');
+
+headerBurger.addEventListener('click', function (e) {
   headerBurger.classList.toggle('active');
   headerMenu.classList.toggle('active');
+  body.classList.toggle('lock');
+});
+
+logo.addEventListener('click', function () {
+  if (headerBurger.classList.contains('active')) {
+    headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    body.classList.toggle('lock');
+  }
 });
--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,6 +1,25 @@
+// const headerBurger = document.querySelector('.header__burger');
+// const headerMenu = document.querySelector('.header__menu');
+// headerBurger.addEventListener('click', function () {
+//   headerBurger.classList.toggle('active');
+//   headerMenu.classList.toggle('active');
+// });
+
 const headerBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
+const logo = document.querySelector('.header__logo');
+const body = document.querySelector('body');
+
 headerBurger.addEventListener('click', function () {
   headerBurger.classList.toggle('active');
   headerMenu.classList.toggle('active');
+  body.classList.toggle('lock');
+});
+
+logo.addEventListener('click', function () {
+  if (headerBurger.classList.contains('active')) {
+    headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    body.classList.toggle('lock');
+  }
 });
