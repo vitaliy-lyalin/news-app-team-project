80debcb16e407ca746938d2ad0a045a2568d1d18
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-03-03 22:44:47 +0200

    fix filterByDate

--- src/js/fetchData/filters.js
+++ src/js/fetchData/filters.js
@@ -1,17 +1,16 @@
-// async function fetchCategories() {
-//   const BASE_URL = 'https://api.nytimes.com/svc/news/v3/content/';
+// async function fetchMostViwedCategories() {
+//   const BASE_URL = 'https://api.nytimes.com/svc/mostpopular/v2/';
 //   const API_KEY = 'api-key=1H8y2dY2rihC7fdcuGY6W6JByrUaIDi7';
-//   const url = `${BASE_URL}section-list.json?${API_KEY}`;
+//   const url = `${BASE_URL}viewed/7.json?${API_KEY}`;
 
 //   const response = await fetch(url);
 //   const responseJson = await response.json();
-//   const categories = responseJson.results;
-//   const categoriesName = categories.map(i => i);
-//   // console.log('categoriesName:', categoriesName);
+//   const popularCategories = responseJson.results;
+//   console.log('popularCategories:', popularCategories);
 
-//   return categoriesName;
+//   return popularCategories;
 // }
-// // fetchCategories();
+// fetchMostViwedCategories();
 
 const news = document.querySelector('.news');
 
@@ -36,32 +35,37 @@ export async function fetchChosenCategorie(categorieValue) {
   }
 }
 
-// async function filterByDate() {
-//   try {
-//     const categorie = await fetchChosenCategorie();
-//     const updateDate = categorie.map(i => {
-//       return i.updated_date;
-//     });
-//     // console.log(updateDate.sort().reverse());
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// // filterByDate();
+async function filterByDate(selectedDate) {
+  try {
+    // const categorie = await fetchChosenCategorie();
+    const popularCategories = await fetchMostViwedCategories();
+    const filterPopularCategories = popularCategories.map(i => {
+      return i.filter(({ update_date }) => {
+        return update_date === selectedDate;
+      });
+    });
+    console.log(filterPopularCategories);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+filterByDate();
 
 function renderingNewsNotFound() {
   return `<section class="news-not-found--section">
     <div class="news-not-found--container">
       <h2 class="news-not-found--title">We haven't found news from this category</h2>
-      <picture>
-        <source srcset="/src/images/newsNotFound/notFoundNews_image_mobile_1x.jpg 1x,
-                        /src/images/newsNotFound/notFoundNews_image_mobile_2x.jpg 2x" media="(max-width: 767px)">
-        <source srcset="/src/images/newsNotFound/notFoundNews_image_tablet_1x.jpg 1x,
-                        /src/images/newsNotFound/notFoundNews_image_tablet_2x.jpg 2x" media="(min-width: 768px)">
-        <source srcset="/src/images/newsNotFound/notFoundNews_image_desktop_1x.jpg 1x,
-                        /src/images/newsNotFound/notFoundNews_image_desktop_2x.jpg 2x" media="(min-width: 1200px)">
-        <img class="news-not-found--img" src="/src/images/newsNotFound/notFoundNews_image_mobile_1x.jpg" alt="newspaper on the background of nature" width="250" />
-      </picture>
+      <img class="news-not-found--img" src="./images/newsNotFound/notFoundNews_image_mobile_1x.jpg" alt="newspaper on the background of nature" width="250" />
     </div>
   </section>`;
 }
+
+// {/* <picture>
+//         <source srcset="./src/images/newsNotFound/notFoundNews_image_mobile_1x.jpg 1x,
+//                         ./src/images/newsNotFound/notFoundNews_image_mobile_2x.jpg 2x" media="(max-width: 767px)">
+//         <source srcset="./src/images/newsNotFound/notFoundNews_image_tablet_1x.jpg 1x,
+//                         ./src/images/newsNotFound/notFoundNews_image_tablet_2x.jpg 2x" media="(min-width: 768px)">
+//         <source srcset="./src/images/newsNotFound/notFoundNews_image_desktop_1x.jpg 1x,
+//                         ./src/images/newsNotFound/notFoundNews_image_desktop_2x.jpg 2x" media="(min-width: 1200px)">
+//         <img class="news-not-found--img" src="./src/images/newsNotFound/notFoundNews_image_mobile_1x.jpg" alt="newspaper on the background of nature" width="250" />
+//       </picture> */}
