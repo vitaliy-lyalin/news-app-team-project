eb64d9d51d7ef14b8317c6d6853a7ba1aaa90686
Nikita_Beliy <beliynikita1995@gmail.com>
2023-03-04 14:44:41 +0200

    Update createCardsMarkup.js

--- src/js/createCardsMarkup.js
+++ src/js/createCardsMarkup.js
@@ -1,13 +1,20 @@
 export function createCardsMarkup(data) {
+  // console.log(data);
   return data
     .map(({ title, abstract, media, published_date, url, section }) => {
-      const img = media[0]['media-metadata'][2].url;
-      // console.log(media)
+      // console.log(media);
+
+      const noImgUrl =
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
+
+      const img = media.length ? media[0]['media-metadata'][2].url : noImgUrl;
+      // console.log(img);
       return `<div class = "card">
         <div class = "card-img-wrapper">
-        <img class="card__img" src='${img}' alt="" width="350px" height="500px">
+        <img class="card__img" src=${img} alt="" width="350px" height="500px">
         <span class="card__btn">add/remove</span>
         <span class="card__category">${section}</span>
+        
         </div>
         <h3 class="card__title">${title}</h3>
         <p class="card__text">${abstract}</p>
@@ -16,3 +23,5 @@ export function createCardsMarkup(data) {
     })
     .join('');
 }
+
+// ['media-metadata'][2].url
