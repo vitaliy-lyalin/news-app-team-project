6dd7c6857ee44653b09e062d83346746eeb22aa8
oolga77 <oemi.ua@gmail.com>
2023-03-06 14:28:34 +0200

    fixed calendar

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -2,13 +2,29 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const datePicker = document.querySelector('#date-picker');
+const dateInput = document.querySelector('.date-input')
+const dateUp = document.querySelector('.date-up');
+const dateDown = document.querySelector('.date-down')
+const calendarSvg = document.querySelector('.icon-Frame');
+let selectedDate;
 
 const options = {
   dateFormat: 'd/m/Y',
   locale: 'en',
   onChange: function (selectedDates) {
-    const selectedDate = selectedDates[0];
+    selectedDate = selectedDates[0];
+   
   },
 };
-
 flatpickr(datePicker, options);
+
+dateInput.addEventListener('click', changeSvg)
+  function changeSvg(){
+    if(selectedDate > 0) {
+      dateInput.style.backgroundcolor = '#4440F6';
+    }
+    //dateDown.classList.add('active');
+    //dateInput.classList.add('active');
+    calendarSvg.style.fill('#F8F8F8')
+  }
+
