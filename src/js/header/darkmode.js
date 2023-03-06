869f3245247abbfc65f0089f4318c87b58417a01
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-03-06 22:10:27 +0200

    Add Dark/Light theme toggle

--- src/js/header/darkmode.js
+++ src/js/header/darkmode.js
@@ -1,56 +1,82 @@
-window.addEventListener("load", windowLoad);
+window.addEventListener('load', windowLoad);
 
 function windowLoad() {
+  const htmlBlock = document.documentElement;
 
-    const htmlBlock = document.documentElement;
+  const saveUserTheme = localStorage.getItem('user-theme');
 
-    const saveUserTheme = localStorage.getItem('user-theme');
-
-    let userTheme;
-    if (window.matchMedia) {
-        userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    }
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-        !saveUserTheme ? changeTheme() : null;
+  let userTheme;
+  if (window.matchMedia) {
+    userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  }
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', e => {
+      !saveUserTheme ? changeTheme() : null;
     });
+  // ===== Закоментовано при заміні розмітки та оформлення перемикача та замінено на код нижче
+  //   const themeButton = document.querySelector('.page__theme');
+  //   const resetButton = document.querySelector('.page__reset');
+  //   if (themeButton) {
+  //     themeButton.addEventListener('click', function (e) {
+  //       resetButton.classList.add('active');
+  //       changeTheme(true);
+  //     });
+  //   }
+  //   if (resetButton) {
+  //     resetButton.addEventListener('click', function (e) {
+  //       resetButton.classList.remove('active');
+  //       localStorage.setItem('user-theme', '');
+  //     });
+  // =============================
+  toggle.addEventListener('input', e => {
+    const isChecked = e.target.checked;
 
-    const themeButton = document.querySelector('.page__theme');
-    const resetButton = document.querySelector('.page__reset');
-    if (themeButton) {
-        themeButton.addEventListener("click", function (e) {
-            resetButton.classList.add('active');
-            changeTheme(true);
-        });
+    if (isChecked) {
+      changeTheme(true);
+    } else {
+      localStorage.setItem('user-theme', '');
     }
-    if (resetButton) {
-        resetButton.addEventListener("click", function (e) {
-            resetButton.classList.remove('active');
-            localStorage.setItem('user-theme','');
-        });
+  });
+
+  function setThemeClass() {
+    if (saveUserTheme) {
+      htmlBlock.classList.add(saveUserTheme);
+      //   resetButton.classList.add('active');  Закоментовано при заміні розмітки та оформлення перемикача
+    } else {
+      htmlBlock.classList.add(userTheme);
     }
+  }
 
-    function setThemeClass() {
-        if (saveUserTheme) {
-            htmlBlock.classList.add(saveUserTheme)
-            resetButton.classList.add('active');
-        } else {
-            htmlBlock.classList.add(userTheme);
-        }
+  setThemeClass();
+
+  function changeTheme(saveTheme = false) {
+    let currentTheme = htmlBlock.classList.contains('light') ? 'light' : 'dark';
+    let newTheme;
+
+    if (currentTheme === 'light') {
+      newTheme = 'dark';
+    } else if (currentTheme === 'dark') {
+      newTheme = 'light';
     }
-
-    setThemeClass();
-
-    function changeTheme(saveTheme = false) {
-        let currentTheme = htmlBlock.classList.contains('light') ? 'light' : 'dark';
-        let newTheme;
-
-        if (currentTheme === 'light') {
-            newTheme = 'dark';
-        } else if (currentTheme === 'dark') {
-            newTheme = 'light';
-        }
-        htmlBlock.classList.remove(currentTheme);
-        htmlBlock.classList.add(newTheme);
-        saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
-    }
+    htmlBlock.classList.remove(currentTheme);
+    htmlBlock.classList.add(newTheme);
+    saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
+  }
 }
+
+// ====== Приклад використарння перемикача ======
+// const toggle = document.getElementById('toggle');
+// const body = document.body;
+
+// toggle.addEventListener('input', e => {
+//   const isChecked = e.target.checked;
+
+//   if (isChecked) {
+//     body.classList.add('dark-theme');
+//   } else {
+//     body.classList.remove('dark-theme');
+//   }
+// });
