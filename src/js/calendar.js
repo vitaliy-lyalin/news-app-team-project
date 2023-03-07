9662d5b0f88a3f5e60c42f96c5ce2717199501d1
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-03-07 21:18:48 +0200

    fix

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -7,18 +7,18 @@ const datePicker = document.querySelector('#date-picker');
 const options = {
   dateFormat: 'd/m/Y',
   locale: 'en',
-  onChange: function (selectedDates) {
-    const selectedDate = selectedDates[0];
-    // console.log('selectedDate:', selectedDate);
-    // datePicker.addEventListener('change', async e => {
-    //   const selectedDate = e.target.value;
+  // onChange: function (selectedDates) {
+  //   const selectedDate = selectedDates[0];
+  //   // console.log('selectedDate:', selectedDate);
+  //   // datePicker.addEventListener('change', async e => {
+  //   //   const selectedDate = e.target.value;
 
-    //   const formattedDate = selectedDate.split('/').reverse().join('-');
-    //   console.log('formattedDate:', formattedDate);
-    //   const filterByDateViwed = await filterByDateMostViwed(formattedDate);
-    //   return filterByDateViwed;
-    // });
-  },
+  //   //   const formattedDate = selectedDate.split('/').reverse().join('-');
+  //   //   console.log('formattedDate:', formattedDate);
+  //   //   const filterByDateViwed = await filterByDateMostViwed(formattedDate);
+  //   //   return filterByDateViwed;
+  //   // });
+  // },
 };
 
 flatpickr(datePicker, options);
