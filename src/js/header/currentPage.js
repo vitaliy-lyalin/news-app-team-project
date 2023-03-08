7dbc93aaa0ee59bd198f81a210a7fb546201756f
Kate Kolosovska-Shchudlo <kate.shchudlo@gmail.com>
2023-03-08 16:40:39 +0200

    add addActiveClassToCurrentPage() to pages

--- src/js/header/currentPage.js
+++ src/js/header/currentPage.js
@@ -1,26 +1,20 @@
 export function addActiveClassToCurrentPage() {
-  let links = document.querySelectorAll('.header__link');
-  let bodyId = document.querySelector('body').id;
+  // let links = document.querySelectorAll('.header__link');
+  // let bodyId = document.querySelector('body').id;
 
   // for (let link of links) {
   //   if (link.dataset.active == bodyId) {
   //     link.classList.add('current');
   //   }
   // }
-  for (let i = 0; i < links.length; i++) {
-    let link = links[i];
-    if (link.dataset.active === bodyId) {
+
+  const currentUrl = window.location.href;
+  const menuLinks = document.querySelectorAll('.header__link');
+
+  menuLinks.forEach(link => {
+    const linkUrl = link.href;
+    if (currentUrl === linkUrl) {
       link.classList.add('current');
     }
-  }
-
-  // const currentUrl = window.location.href;
-  // const menuLinks = document.querySelectorAll('.header__link');
-
-  // menuLinks.forEach(link => {
-  //   const linkUrl = link.href;
-  //   if (currentUrl === linkUrl) {
-  //     link.classList.add('current');
-  //   }
-  // });
+  });
 }
