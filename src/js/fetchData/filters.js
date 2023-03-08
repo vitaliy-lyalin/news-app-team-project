dd17e660407f3d247db161142ecde912587f7b0b
Dimon <bolilui.dmytro@gmail.com>
2023-03-08 12:41:38 +0200

    Fixed filterByChosenCategorie.js

--- src/js/fetchData/filters.js
+++ src/js/fetchData/filters.js
@@ -6,42 +6,42 @@ import { createCardsMarkupNoBackend } from '../createCardsMarkupNoBackend';
 const card__containerEl = document.querySelector('.card-container');
 const weatherContainer = document.querySelector('.weather__container');
 
-export async function filterByDateMostViwed(e) {
-  const dateFromInput = e.target.value;
-  // console.log('dateFromInput:', dateFromInput);
-  const transformDate = dateFromInput.split('/').reverse().join('-');
-  // console.log('transformDate:', transformDate);
+// export async function filterByDateMostViwed(e) {
+//   const dateFromInput = e.target.value;
+//   // console.log('dateFromInput:', dateFromInput);
+//   const transformDate = dateFromInput.split('/').reverse().join('-');
+//   // console.log('transformDate:', transformDate);
 
-  if (!dateFromInput) {
-    return;
-  } else {
-    const API_KEY = '1H8y2dY2rihC7fdcuGY6W6JByrUaIDi7';
-    const MOST_POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?&api-key=${API_KEY}`;
+//   if (!dateFromInput) {
+//     return;
+//   } else {
+//     const API_KEY = '1H8y2dY2rihC7fdcuGY6W6JByrUaIDi7';
+//     const MOST_POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?&api-key=${API_KEY}`;
 
-    const response = await fetch(`${MOST_POPULAR_URL}`);
-    const responseJson = await response.json();
-    const results = responseJson.results;
+//     const response = await fetch(`${MOST_POPULAR_URL}`);
+//     const responseJson = await response.json();
+//     const results = responseJson.results;
 
-    try {
-      const filterByDatePopularNews = results.filter(i => {
-        return i.published_date == transformDate;
-      });
-      if (filterByDatePopularNews.length === 0) {
-        weatherContainer.style.display = 'none';
-        card__containerEl.style.display = 'block';
-        card__containerEl.innerHTML = renderingNewsNotFound();
-      } else {
-        weatherContainer.style.display = 'block';
-        card__containerEl.style.display = 'grid';
-        card__containerEl.innerHTML = createCardsMarkupNoBackend(
-          filterByDatePopularNews
-        );
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
+//     try {
+//       const filterByDatePopularNews = results.filter(i => {
+//         return i.published_date == transformDate;
+//       });
+//       if (filterByDatePopularNews.length === 0) {
+//         weatherContainer.style.display = 'none';
+//         card__containerEl.style.display = 'block';
+//         card__containerEl.innerHTML = renderingNewsNotFound();
+//       } else {
+//         weatherContainer.style.display = 'block';
+//         card__containerEl.style.display = 'grid';
+//         card__containerEl.innerHTML = createCardsMarkupNoBackend(
+//           filterByDatePopularNews
+//         );
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
 
 export async function filterByChosenCategorie(categorieValue) {
   const BASE_URL = 'https://api.nytimes.com/svc/news/v3/content/';
@@ -60,8 +60,7 @@ export async function filterByChosenCategorie(categorieValue) {
     } else {
       weatherContainer.style.display = 'block';
       card__containerEl.style.display = 'grid';
-      card__containerEl.innerHTML = createCategorieCardMarkup(chosenCategorie);
-
+      card__containerEl.innerHTML = createCategoriesCardMarkup(chosenCategorie);
 
       // console.log(chosenCategorie);
     }
@@ -70,12 +69,12 @@ export async function filterByChosenCategorie(categorieValue) {
   }
 }
 
-// async function filterByDateCategorie() {
-//   try {
-//     const categorie = await filterByChosenCategorie(chosenCategorie);
-//     console.log('categorie:', categorie);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// filterByDateCategorie();
+// // async function filterByDateCategorie() {
+// //   try {
+// //     const categorie = await filterByChosenCategorie(chosenCategorie);
+// //     console.log('categorie:', categorie);
+// //   } catch (error) {
+// //     console.log(error.message);
+// //   }
+// // }
+// // filterByDateCategorie();
